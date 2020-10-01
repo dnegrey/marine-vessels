312264f387ee503b6021cc6f880171301a06f79b
Dan Negrey <dnegrey@gmail.com>
2020-10-01 16:30:56 -0400

    added conditional fetch and read of ships data

--- src/R/main/getData.R
+++ src/R/main/getData.R
@@ -1,2 +1,24 @@
 getData <- function(dir) {
+    # download ships data, if it does not already exist
+    if (!file.exists("var/ships.csv")) {
+        download.file(
+            "https://drive.google.com/uc?export=download&id=160JnqoQysqzvR1GBBnKJFKAew_v6TYli",
+            "var/ships_data.zip"
+        )
+        unzip(
+            "var/ships_data.zip",
+            exdir = "var"
+        )
+    }
+    # read in ships data
+    RDataCreate(
+        dir = dir,
+        x = "ships",
+        fun = read.csv,
+        args = list(
+            file = "var/ships.csv",
+            na.strings = "",
+            stringsAsFactors = FALSE
+        )
+    )
 }
