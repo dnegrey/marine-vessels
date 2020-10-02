4c9ce7180c20e43099dead4db72f4c05c926ac2d
Dan Negrey <dnegrey@gmail.com>
2020-10-02 14:20:16 -0400

    initial main data object

--- src/R/main/analyzeData.R
+++ src/R/main/analyzeData.R
@@ -9,12 +9,52 @@ analyzeData <- function(dir) {
             xstn = RDataUse(dir, "cleanShipTypeName")
         )
     )
+    # find observation with longest distance sailed
+    RDataCreate(
+        dir = dir,
+        x = "mainShipsMaxDistance",
+        fun = function(x) {
+            # main logic for finding max distance/de-duping
+            y <- x %>%
+                filter(!is.na(DistSinceLast_Meters)) %>%
+                group_by(ShipTypeNameId) %>%
+                summarise(DistSinceLast_Meters = max(DistSinceLast_Meters)) %>%
+                data.frame() %>%
+                inner_join(x, c("ShipTypeNameId", "DistSinceLast_Meters")) %>%
+                group_by(ShipTypeNameId) %>%
+                summarise(ShipsRecordId = max(ShipsRecordId)) %>%
+                data.frame() %>%
+                select(ShipsRecordId) %>%
+                inner_join(x, "ShipsRecordId")
+            # include ships that had only one obs (can exclude later)
+            yo <- x %>%
+                anti_join(select(y, ShipTypeNameId), "ShipTypeNameId")
+            # combine
+            z <- rbind(y, yo) %>%
+                arrange(ShipTypeNameId)
+            row.names(z) <- NULL
+            stopifnot(
+                nrow(z) == length(unique(z$ShipTypeNameId)),
+                nrow(z) == length(unique(z$ShipsRecordId)),
+                all(z$ShipTypeNameId %in% x$ShipTypeNameId)
+            )
+            return(z)
+        },
+        args = list(
+            x = RDataUse(dir, "mainShips")
+        )
+    )
     # main data object
     RDataCreate(
         dir = dir,
         x = "main",
         fun = list,
         args = list(
+            shipTypeName = {
+                x = RDataUse(dir, "cleanShipTypeName")
+                split(x, x$ShipType)
+            },
+            shipMaxDist = RDataUse(dir, "mainShipsMaxDistance")
         )
     )
 }
